3b0043f9f8b75c41b15b40ae57490e14869c17de
Anurag Yadav <yadavanurag307@gmail.com>
2021-12-16 19:38:35 +0530

    Initial commit of firebase push notification

---
+++ app/src/main/java/com/example/firebasepushnotification2/MyFirebaseMessagingService.java
@@ -0,0 +1,34 @@
+package com.example.firebasepushnotification2;
+
+import android.app.NotificationChannel;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+         getFirebaseMessage(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+
+    }
+
+    public void getFirebaseMessage(String title, String msg){
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"myFirebasechannel ")
+                .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
+                .setContentTitle(title)
+                .setContentText(msg)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+        manager.notify(101,builder.build());
+
+
+    }
+}
